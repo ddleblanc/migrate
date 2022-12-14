30966c7778ea46e7296eb0e70d36c1b09984c3f4
Daniel le Blanc <lxblvnc@gmail.com>
2022-12-14 16:44:44 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import Layout from "../components/layout";
 import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
-import Meta from "../components/Meta";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
@@ -21,7 +19,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Meta title="Home" />
       <Provider store={store}>
       <ThirdwebProvider desiredChainId={ChainId.BinanceSmartChainMainnet}>
         <WagmiConfig client={client}>
